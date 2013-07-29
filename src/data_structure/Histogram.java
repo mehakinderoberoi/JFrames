b32ed4e6a39fffc9acc406353cb83d12d428d867
Allen Liu <zliu224@wisc.edu>
2013-07-29 01:23:49 -0500

    adding template for histagram

--- src/data_structure/Histogram.java
+++ src/data_structure/Histogram.java
@@ -1,5 +1,27 @@
 package data_structure;
 
-public abstract class Histogram {
+/**
+ * API for Histogram data-structure. Super class for all other used histogram
+ * @author allenliu
+ *
+ */
+public abstract class Histogram<E> {
+	
+	/**
+	 * Put an element in Histagram
+	 * @param item
+	 */
+	public abstract void put(E item);
+	
+	/**
+	 * Print the stat for the current Histogram
+	 */
+	public abstract void printStat();
+	
+	/**
+	 * get number of element in the histagram
+	 * @return size of elements
+	 */
+	public abstract int getElementsSize();
 	
 }
